b5a13a91d90feb071d5ad2b396581a55ae086bfe
Ravi Teja <iraviteja77@gmail.com>
2018-06-12 16:43:14 +0530

    routing using promises

--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,39 @@ const User = require("../models/user.js");
 
 router
   .route("/")
+  //   .get((req, res, next) => {
+  //     User.find({}, (err, users) => {
+  //       if (err) throw err;
+  //       console.log(users);
+  //     });
+  //     res.status(200).json({ message: "This is home router" });
+  //   })
   .get((req, res, next) => {
-    User.find({}, (err, users) => {
-      if (err) throw err;
-      console.log(users);
-    });
-    res.status(200).json({ message: "This is home router" });
+    User.find({})
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => {
+        next(err);
+      });
   })
-  .post((req, res, next) => {
+  //   .post((req, res, next) => {
+  //     const newuser = new User(req.body);
+  //     newuser.save((err, user) => {
+  //       if (err) throw err;
+  //       res.json(user);
+  //     });
+  //   });
+  .post((req, res, err) => {
     const newuser = new User(req.body);
-    newuser.save((err, user) => {
-      if (err) throw err;
-      res.json(user);
-    });
+    newuser
+      .save()
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        next(err);
+      });
   });
 
 module.exports = router;
